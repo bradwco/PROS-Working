21d64c59eabdc90bad6590ad48b3159a08a95014
Bradley Co <bradco626@gmail.com>
2025-02-25 09:19:46 -0800

    Added to the blue negative auton *NOT WORKING CONSISTENTLY*

--- src/autonPaths.cpp
+++ src/autonPaths.cpp
@@ -5,6 +5,25 @@
 
 void blueNegative(){
     pros::lcd::print(3, "%s", "BLUE NEG");
+
+    chassis.setPose(tileSize*4-xOffset,tileSize-yOffset,0);
+    chassis.moveToPose(tileSize*4-xOffset, tileSize*2, 323, 1500, {.minSpeed=70, .earlyExitRange=10});
+    chassis.waitUntilDone();
+    chassis.cancelAllMotions();
+
+    left_motor_group.move(20);
+    right_motor_group.move(20);
+    pros::delay(4000);
+    left_motor_group.brake();
+    right_motor_group.brake();
+
+    left_motor_group.move(-100);
+    right_motor_group.move(-100);
+    pros::delay(500);
+    left_motor_group.brake();
+    right_motor_group.brake();
+
+    chassis.moveToPose(tileSize*4-xOffset,tileSize-yOffset,0, 3000, {.forwards=false});
 }
 
 void redNegative(){
